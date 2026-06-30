a33b1d4a31c905cd1abf883baa4ff87de497e5b8
felipe-damasceno19 <felipegpt19@gmail.com>
2026-06-30 09:09:14 -0300

    chore: initial commit

---
+++ src/main/java/io/github/felipe_damasceno19/app/AppApplication.java
@@ -0,0 +1,13 @@
+package io.github.felipe_damasceno19.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/felipe_damasceno19/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipe_damasceno19.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
